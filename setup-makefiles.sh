07b4f295723e79aec3bbfe51524918eac7a82792
Alexander Koskovich <akoskovich@pm.me>
2022-07-20 09:16:53 -0700

    sm8250-common: Move BT profiles props to vendor
     * Makes it GSI compatible
    
    Change-Id: I8f7caf241d508af59c16482e6185c42f23f8a460

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -7,7 +7,6 @@
 #
 
 set -e
-
 # Load extract_utils and do some sanity checks
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
@@ -33,15 +32,15 @@ write_makefiles "${MY_DIR}/proprietary-files.txt" true
 # Finish
 write_footers
 
-if [ -s "${MY_DIR}/../${DEVICE}/proprietary-files.txt" ]; then
+if [ -s "${MY_DIR}/../$DEVICE/proprietary-files.txt" ]; then
     # Reinitialize the helper for device
-    setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false
+    setup_vendor "$DEVICE" "${VENDOR}" "${ANDROID_ROOT}" false
 
     # Warning headers and guards
     write_headers
 
     # The standard device blobs
-    write_makefiles "${MY_DIR}/../${DEVICE}/proprietary-files.txt" true
+    write_makefiles "${MY_DIR}/../$DEVICE/proprietary-files.txt" true
 
     # Finish
     write_footers
