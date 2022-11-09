28feb02761836f6a127872a8c0b71e824a04c0df
Andrea055 <vivanco11.ac@gmail.com>
2022-11-09 09:29:56 +0100

    remove selinux from tree and switch to sepolicy_vndr-legacy-um

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -218,15 +218,9 @@ ENABLE_VENDOR_RIL_SERVICE := true
 VENDOR_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
 
 # SELinux
-#SELINUX_IGNORE_NEVERALLOWS := true
-#include device/qcom/sepolicy_vndr/SEPolicy.mk
-#include hardware/oplus/sepolicy/qti/SEPolicy.mk
-
-# Sepolicy
-BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
-SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
-SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/public
-
+SELINUX_IGNORE_NEVERALLOWS := true
+include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
+include hardware/oplus/sepolicy/qti/SEPolicy.mk
 
 # Verified boot
 BOARD_AVB_ENABLE := true
